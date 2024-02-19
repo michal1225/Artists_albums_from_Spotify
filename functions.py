b916df7b8e2156d73ec6a5fe6cca1a35793c6bb6
Michał Krawczyński <michal.krawczynski@icloud.com>
2024-02-19 20:23:12 +0100

    checking if .json format is correct

--- functions.py
+++ functions.py
@@ -6,14 +6,20 @@ import pandas as pd
 class Export:
     @staticmethod
     def to_json(data, file_name):
-        with open(file_name, 'w') as file:
-            json.dump(data, file, indent=4)
+        if file_name[-1] == 'n' and file_name[-2] == 'o' and file_name[-3] == 's' and file_name[-4] == 'j' and file_name[-5] == '.':
+            with open(file_name, 'w') as file:
+                json.dump(data, file, indent=4)
+            print("File saved correctly")
+        else:
+            print("Invalid file name")
+            return
 
     @staticmethod
     def to_excel(data, file_name):
         df = pd.DataFrame(data)
         try:
             df.to_excel(file_name, engine='openpyxl', index=False)
+            print("File saved correctly")
         except:
             print("Error while writing to Excel file.")
 
@@ -22,6 +28,7 @@ class Export:
         df = pd.DataFrame(data)
         try:
             df.to_csv(file_name, index=False)
+            print("File saved correctly")
         except:
             print("Error while writing to CSV file.")
 
